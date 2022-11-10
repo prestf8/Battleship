b88b8038ea66957add1db1c25a28535155b87e34
prestf8 <prestf8@gmail.com>
2022-11-10 11:38:11 -0800

    board DOM setup

--- DOM.js
+++ DOM.js
@@ -1,3 +1,24 @@
 const DOM = (() => {
+    let playerBoardDOM, computerBoardDOM;
+    let initialization = () => {
+        playerBoardDOM= document.querySelector(".player-board");
+        computerBoardDOM= document.querySelector(".computer-board");
+
+        for(let i=0; i < 100; i++) {
+            let playerBoardDiv = document.createElement("div");
+            let computerBoardDiv = document.createElement("div");
+            playerBoardDiv.setAttribute("class", "board-div player-board-div");
+            computerBoardDiv.setAttribute("class", "board-div computer-board-div");
+            playerBoardDiv.setAttribute("data-coordinate", i+1);
+            computerBoardDiv.setAttribute("data-coordinate", i+1);
+            playerBoardDOM.appendChild(playerBoardDiv);
+            computerBoardDOM.appendChild(computerBoardDiv);
+        }
+    }
+    return {
+        initialization
+    }
     
-})
+})()
+
+DOM.initialization();
