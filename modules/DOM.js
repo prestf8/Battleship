4e8757a511d6d6180918adef45a46b1c685dd7fc
prestf8 <prestf8@gmail.com>
2022-11-13 14:40:12 -0800

    hover place ship function works unedited version 2

--- modules/DOM.js
+++ modules/DOM.js
@@ -1,40 +1,69 @@
 const DOM = (() => {
     let playerBoardDOM, computerBoardDOM;
     let placeShipLength = 5; // Length of ship to place
-    // let horizontal = true;
+    let playerBoardDivs;
+    let horizontal = true;
 
     let clickBoardUnit = (event) => {
         // console.log(event.target.getAttribute("data-coordinate"))
-    }
-
-    let hoverShip = (event) => {
-        for(let boardUnitDOM of playerBoardDOM.querySelectorAll(".player-board-div")) {
-            boardUnitDOM.classList.remove("board-div-selected");
-        }
-
-        let hoveredCoordinate = parseInt(event.target.getAttribute("data-coordinate"));
-        let row = event.target.getAttribute("data-coordinate")[0];
-        console.log(row);
-        let toBeSelectedCoordinates = [];
-        for(let i=0; i < placeShipLength; i++) { // placeShipLength - 1 not including hoveredCoordinate
-            let newHoveredCoordinate = hoveredCoordinate + i; 
-            // hovered coordinate shouldnt be off the board && should be on the same row
-            if (newHoveredCoordinate <= 100 && newHoveredCoordinate.toString()[0] === row) { 
-                toBeSelectedCoordinates.push(hoveredCoordinate + i);
-            }
-        }
-
-        for(let coord of toBeSelectedCoordinates) {
-            let boardUnit = playerBoardDOM.querySelector(`[data-coordinate="${coord}"]`);
-            boardUnit.classList.add("board-div-selected");
-        }
-        // console.log(toBeSelectedCoordinates);
     }
 
     let getHorizontal = () => horizontal;
 
     let toggleHorizontal = () => {
         horizontal = !horizontal;
+    }
+
+    let storeShipPlacementH = (boardUnit, hoveredCoordinate) => {
+        let selectedCoordinates = [];
+        let row = boardUnit.getAttribute("data-coordinate")[0];
+        for(let i=0; i < placeShipLength; i++) { // placeShipLength - 1 not including hoveredCoordinate
+            let selectedCoordinate = hoveredCoordinate + i; 
+            let selectedCoordinateStr = (hoveredCoordinate+i).toString(); // string version
+
+            if (selectedCoordinate < 10) {
+                selectedCoordinateStr = '0' + selectedCoordinateStr;
+            }
+
+            // selected coordinate shouldnt be off the board && should be on the same row
+            if (selectedCoordinate <= 100 && selectedCoordinateStr.toString()[0] === row) { 
+                selectedCoordinates.push(selectedCoordinateStr);
+            }
+        }
+        return selectedCoordinates;
+    }
+
+    let storeShipPlacementV = (boardUnit, hoveredCoordinate) => {
+        let selectedCoordinates = [];
+        let column = boardUnit.getAttribute("data-coordinate")[0];
+        for(let i=0; i < placeShipLength; i++) { // placeShipLength - 1 not including hoveredCoordinate
+            let selectedCoordinate = hoveredCoordinate + i; 
+
+            // selected coordinate shouldnt be off the board && should be on the same row
+            if (selectedCoordinate <= 100 && selectedCoordinate.toString()[0] === row) { 
+                selectedCoordinates.push(selectedCoordinate);
+            }
+        }
+        return selectedCoordinates;
+    }
+
+    let hoverShipPlacement = (event) => {
+        let hoveredBoardUnit = event.target;
+        let hoveredCoordinate = parseInt(hoveredBoardUnit.getAttribute("data-coordinate"));
+
+        for(let boardUnit of playerBoardDivs) { 
+            boardUnit.classList.remove("board-div-selected");
+        }
+
+        let shipPlacement;
+
+        shipPlacement = storeShipPlacementH(hoveredBoardUnit, hoveredCoordinate);
+
+        for(const coord of shipPlacement) {
+            let boardUnit = playerBoardDOM.querySelector(`[data-coordinate="${coord}"]`);
+            boardUnit.classList.add("board-div-selected");
+        }
+        // console.log(toBeSelectedCoordinates);
     }
 
     let initialization = () => {
@@ -49,22 +78,26 @@ const DOM = (() => {
             let computerBoardDiv = document.createElement("div");
             playerBoardDiv.setAttribute("class", "board-div player-board-div");
             computerBoardDiv.setAttribute("class", "board-div computer-board-div");
-            playerBoardDiv.setAttribute("data-coordinate", i+1);
-            computerBoardDiv.setAttribute("data-coordinate", i+1);
+
+            let dataCoordinateValue = String(i+1);
+            if (parseInt(dataCoordinateValue) < 10) {
+                dataCoordinateValue = '0' + dataCoordinateValue;
+            }
+            playerBoardDiv.setAttribute("data-coordinate", dataCoordinateValue);
+            computerBoardDiv.setAttribute("data-coordinate", dataCoordinateValue);
             playerBoardDOM.appendChild(playerBoardDiv);
             computerBoardDOM.appendChild(computerBoardDiv);
         }
+
+        // store the newly generated board units 
+        playerBoardDivs = document.querySelectorAll(".player-board-div");
         initializeHoverAndPlaceShips();
     }
 
     let initializeHoverAndPlaceShips = () => {
-        let playerBoardDivs = document.querySelectorAll(".player-board-div");
-        let computerBoardDivs = document.querySelectorAll(".computer-board-div");
         for(let i=0; i < 100; i++) {            
-            playerBoardDivs[i].addEventListener("mouseover", hoverShip);
-            // computerBoardDivs[i].addEventListener("mouseover", hoverShip);
+            playerBoardDivs[i].addEventListener("mouseover", hoverShipPlacement);
             playerBoardDivs[i].addEventListener("click", clickBoardUnit);
-            // computerBoardDivs[i].addEventListener("click", clickBoardUnit);
         }        
     }
     return {
